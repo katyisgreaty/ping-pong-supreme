7a55d38ae20c6d9505d56d631aba5c848433d2ca
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-01-20 09:02:19 -0800

    attempt javascript function to count up to submitted number

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
+var sequence = function(number) {
+  if(number === 1) return "1";
+  return sequence(number - 1) + ", " + number;
+}
+
+var bigPingPong = function(number) {
+
+
+
+
+
+}
+
+
 
 //Front End Logic
-$(document).ready(function(){
-  $("form#pingPongIt").submit(function(event){
+$(document).ready(function() {
+  $("form#pingPongIt").submit(function(event) {
     event.preventDefault();
     var numberInput = $("input#numberEntry").val();
 
